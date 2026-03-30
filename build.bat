938e5843f4ea1d712d9e6bf9722300df07c33f32
sh3dow <miracberktasdemir202@gmail.com>
2026-03-30 13:51:41 +0300

    Enhance build.bat with safer banner and messages

--- build.bat
+++ build.bat
@@ -1,13 +1,18 @@
 @echo off
+setlocal enabledelayedexpansion
 cls
-echo  ____  _ _                ____  _____ 
-echo / ___|(_) |_   _____ _ __|  _ \| ____|
-echo \___ \| | \ \ / / _ \ '__| |_) |  _|  
-echo  ___) | | |\ V /  __/ |  |  __/| |___ 
-echo |____/|_|_| \_/ \___|_|  |_|   |_____|
-echo.
 
-:: mnt klasörünü oluştur (eğer yoksa)
+:: Banner'ı daha güvenli bir yöntemle yazdırıyoruz
+echo  ____  _ _                ____  _____ 
+echo / ___^|(_) ^|_   _____ _ __^|  _ \^| ____^|
+echo \___ \^| ^| \ \ / / _ \ '__^| ^|_) ^|  _^|  
+echo  ___) ^| ^| ^|\ V /  __/ ^|  ^|  __/^| ^|___ 
+echo ^|____/^|_^|_^| \_/ \___/^|_^|  ^|_^|   ^|_____^|
+echo.
+echo [ SilverPE - Build Script ]
+echo ---------------------------------
+
+:: mnt klasörünü kontrol et ve oluştur
 if not exist "mnt" (
     echo [*] Creating 'mnt' directory...
     mkdir mnt
@@ -17,16 +22,21 @@ echo [*] Building x64 (SilverPE)...
 cargo build --release --target x86_64-pc-windows-msvc
 if %ERRORLEVEL% EQU 0 (
     copy /y "target\x86_64-pc-windows-msvc\release\SilverPE.exe" "mnt\SilverPE_x64.exe"
+    echo [+] x64 Build Successful.
+) else (
+    echo [-] x64 Build Failed!
 )
 
+echo.
 echo [*] Building x86 (SilverPE)...
 cargo build --release --target i686-pc-windows-msvc
 if %ERRORLEVEL% EQU 0 (
     copy /y "target\i686-pc-windows-msvc\release\SilverPE.exe" "mnt\SilverPE_x86.exe"
+    echo [+] x86 Build Successful.
+) else (
+    echo [-] x86 Build Failed!
 )
 
 echo.
-echo [+] Done! All binaries moved to 'mnt' folder.
-echo x64: mnt\SilverPE_x64.exe
-echo x86: mnt\SilverPE_x86.exe
+echo [+] Done! Check the 'mnt' folder.
 pause
